e2bbe000b4b1a7ad395286710996451f5fcd022b
Henry D <59034165+jdugue6@users.noreply.github.com>
2023-01-05 11:42:11 -0500

    Add files via upload

---
+++ ex11.js
@@ -0,0 +1,30 @@
+let college = 'York College';
+const myName = 'Jane Doe';
+
+console.log(college);
+console.log(myName);
+
+const favoriteSubject = 'Math';
+//Console.log the text "My name is: " follows by myName variable.
+console.log("My name is:", myName, ", I go to ", college);
+console.log("My name is:" + myName);
+//Console.log the text "My college is: " follows by college variable.
+//Reassign the value stored in favoriteSubject to the string "Coding", is it possible?
+
+//can you console.log(number1) here?
+
+//Declare a new variable named 'number1' and give it the value of 10.
+
+let number1 = 10;
+//Declare a new variable named 'number2' and give it the value of 15.
+let number2 = 15;
+
+//console.log the summation of number1 and number2 in the following format
+//Expected output: The sum is 25
+//console.log the product of number1 and number2 in the following format
+//Expected output: The product is 150
+
+//Declare a new variable named 'check' and give it the value of true (Boolean).
+//console.log check and see the value, try also with false.
+
+
